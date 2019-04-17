f0326e3720ee3f5cf8966ef9a939f3a32add854f
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-17 13:27:25 +0200

    New component: input-fields/taxrates-input/edit

---
+++ addon/components/input-fields/taxrates-input/edit.js
@@ -0,0 +1,48 @@
+import Component from '@ember/component';
+import layout from '../../../templates/components/input-fields/taxrates-input/edit';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
+import InputField from '@lblod/ember-mu-dynamic-forms/mixins/input-field';
+
+export default Component.extend( InputField, {
+  layout,
+  store: service(),
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('taxRates', A());
+    if (this.model) {
+      const value = this.get(`solution.${this.get('model.identifier')}`);
+      this.set('taxRates', value || A());
+    }
+  },
+
+  activeInputStates: computed( 'inputStates.[]', 'value', function() {
+    const inputStates = this.inputStates;
+
+    return inputStates
+      .map(function( inputState ) {
+        // Yield state name for matching validations
+        switch (inputState.validationName) {
+          case "empty":
+            return this.taxRates && this.taxRates.length == 0 && inputState.stateName;
+          default:
+            return undefined;
+        }})
+      // remove all non-truethy validation names
+      .filter( (i) => i );
+  } ),
+
+  actions: {
+    async create(){
+      let taxRate =this.store.createRecord('tax-rate');
+      await taxRate.save();
+      this.taxRates.pushObject(taxRate);
+    },
+
+    async delete(taxRate){
+      this.taxRates.removeObject(taxRate);
+      await taxRate.destroy();
+    }
+  }
+});
