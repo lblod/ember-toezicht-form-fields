fdf6773f03c8090ade84ebded6b3e8d62af7c466
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-17 13:21:36 +0200

    New component: input-fields/input/edit

---
+++ addon/components/input-fields/input/edit.js
@@ -0,0 +1,7 @@
+import layout from '../../../templates/components/input-fields/input/edit';
+import InputFieldComponent from '@lblod/ember-mu-dynamic-forms/components/input-fields/input/edit';
+
+export default InputFieldComponent.extend({
+  layout,
+  classNames: 'u-spacer--bottom--small'
+});
