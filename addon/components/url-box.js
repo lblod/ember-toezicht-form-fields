823def0778975aa594afba3307c0850939c79087
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-17 08:27:33 +0200

    url-box component

---
+++ addon/components/url-box.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+import layout from '../templates/components/url-box';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  layout,
+  store: service(),
+  disabled: false,
+
+  actions: {
+    addUrlField() {
+      let address = this.store.createRecord('file-address');
+      this.urls.pushObject(address);
+    },
+
+    delete(address){
+      this.onDelete(address);
+    }
+  }
+});
