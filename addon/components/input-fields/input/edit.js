147eec96c2c927e7694c115d2b2f5bbf583b44dc
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-23 14:09:31 +0200

    Rely on existing code from mu-dynamic-forms addon

--- addon/components/input-fields/input/edit.js
+++ addon/components/input-fields/input/edit.js
@@ -1,15 +1,7 @@
 import layout from '../../../templates/components/input-fields/input/edit';
 import InputFieldComponent from '@lblod/ember-mu-dynamic-forms/components/input-fields/input/edit';
-import { oneWay } from '@ember/object/computed';
 
 export default InputFieldComponent.extend({
   layout,
   classNames: 'u-spacer--bottom--small',
-  internalValue: oneWay('value'),
-  
-    actions: {
-      update() {
-        this.updateValue( this.internalValue );
-      }
-    }
 });
--- addon/components/input-fields/input/show.js
+++ addon/components/input-fields/input/show.js
@@ -1,8 +1,5 @@
-import Component from '@ember/component';
-import { oneWay } from '@ember/object/computed';
 import layout from '../../../templates/components/input-fields/input/show';
-
-export default Component.extend({
+import InputFieldComponent from '@lblod/ember-mu-dynamic-forms/components/input-fields/input/edit';
+export default InputFieldComponent.extend({
   layout,
-  internalValue: oneWay('value')
 });
