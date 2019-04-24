fc0eab22135909d4643b79b0d707eef652beddec
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-24 09:50:25 +0200

    Utility function to be used by the host app

---
+++ addon/utils/register-me.js
@@ -0,0 +1,29 @@
+import TextEdit from '@lblod/ember-toezicht-form-fields/components/input-fields/text/edit';
+import TextShow from '@lblod/ember-toezicht-form-fields/components/input-fields/text/show';
+import CheckboxEdit from '@lblod/ember-toezicht-form-fields/components/input-fields/checkbox/edit';
+import CheckboxShow from '@lblod/ember-toezicht-form-fields/components/input-fields/checkbox/show';
+import DynamicSelectEdit from '@lblod/ember-toezicht-form-fields/components/input-fields/dynamic-select/edit';
+import DynamicSelectShow from '@lblod/ember-toezicht-form-fields/components/input-fields/dynamic-select/show';
+import InputEdit from '@lblod/ember-toezicht-form-fields/components/input-fields/input/edit';
+import InputShow from '@lblod/ember-toezicht-form-fields/components/input-fields/input/show';
+import YearEdit from '@lblod/ember-toezicht-form-fields/components/input-fields/year/edit';
+import YearShow from '@lblod/ember-toezicht-form-fields/components/input-fields/year/show';
+
+export default function registerMe( appInstance ) {
+  appInstance.register('component:input-fields/text/edit', TextEdit);
+  appInstance.register('component:input-fields/text/show', TextShow);
+
+  appInstance.register('component:input-fields/checkbox/edit', CheckboxEdit);
+  appInstance.register('component:input-fields/checkbox/show', CheckboxShow);
+
+  appInstance.register('component:input-fields/dynamic-select/edit', DynamicSelectEdit);
+  appInstance.register('component:input-fields/dynamic-select/show', DynamicSelectShow);
+
+  appInstance.register('component:input-fields/input/edit', InputEdit);
+  appInstance.register('component:input-fields/input/show', InputShow);
+  
+  appInstance.register('component:input-fields/year/edit', YearEdit);
+  appInstance.register('component:input-fields/year/show', YearShow);
+  debugger;
+  return true;
+}
---
+++ app/utils/register-me.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-toezicht-form-fields/utils/register-me';
